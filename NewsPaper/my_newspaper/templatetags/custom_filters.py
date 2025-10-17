3ead50ae06622e2cd7eff2d15116991d5f63e627
morziaka <morziaka@gmail.com>
2025-10-17 15:02:55 +0300

    Add files via upload

---
+++ NewsPaper/my_newspaper/templatetags/custom_filters.py
@@ -0,0 +1,21 @@
+from django import template
+
+register = template.Library()
+
+UNCENSORED_WORDS = {
+   'европейск': 'e********',
+   'ставк': 'c****',
+   'человек': 'ч******'
+}
+# Регистрируем наш фильтр под именем currency, чтоб Django понимал,
+# что это именно фильтр для шаблонов, а не простая функция.
+@register.filter()
+def censor(value: str):
+   for old, new in UNCENSORED_WORDS.items():
+      value = value.replace(old, new)
+   return value
+
+@register.filter()
+def format_time(value):
+   format_string = '%d %b %Y'
+   return value.strftime(format_string)
--- NewsPaper/my_newspaper/views.py
+++ NewsPaper/my_newspaper/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from datetime import datetime
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class PostsList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'posts'
+    ordering = ['-time_post']
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
